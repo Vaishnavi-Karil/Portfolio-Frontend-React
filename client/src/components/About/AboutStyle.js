4f3d5c6f1e597a862b6eb7cb37bf102780bc256b
Vaishnavi-Karil <vaishnavigkaril@gmail.com>
2023-09-23 16:06:06 +0530

    update the code

--- client/src/components/About/AboutStyle.js
+++ client/src/components/About/AboutStyle.js
@@ -1,1 +1,26 @@
+import styled from "styled-components";
+
+export const AboutContainer = styled.div`
+display: flex; 
+flex-direction: row;
+`;
+
+export const ImageContainer = styled.div`
+  width: 49%;
+`;
+
+export const ProfileImage = styled(Image)`
+  width: 68%;
+  height: 80%;
+  border: transparent;
+  border-radius: 10%;
+  margin: 3.5rem 0rem;
+`;
+
+
+export const DescriptionContainer = styled.div`
+  width: 51%;
+  display: flex;
+`;
+
 
--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {  CardContainer, CardHeadingContainer, Heading1, MoreInformation} from '@/styles/Styles'
+import { DescriptionContainer, ImageContainer, ProfileImage } from './AboutStyle'
 
 export const About = () => {
   return (
@@ -8,6 +9,14 @@ export const About = () => {
       <MoreInformation>Get To Know More</MoreInformation>
         <Heading1>About Me</Heading1>
       </CardHeadingContainer>
+      <AboutContainer>
+      <ImageContainer>
+        <ProfileImage src={profile_pic} alt={profile_pic} />
+      </ImageContainer>
+      <DescriptionContainer>
+      <h1>Hello</h1>
+        </DescriptionContainer>
+      </AboutContainer>
     </CardContainer>
   )
 }
